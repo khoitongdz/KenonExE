f7b7307e835ba59833f9837c70a09ddfd20a655b
khoitongdz <khoitong0000@gmail.com>
2024-12-05 13:10:41 +0700

    Update Kenon_Hub_Fixed.lua

--- Kenon_Hub_Fixed.lua
+++ Kenon_Hub_Fixed.lua
@@ -1,4 +1,3 @@
-
 -- Kenon Hub Script
 
 if not game:IsLoaded() then
@@ -9,12 +8,12 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("Kenon Hub", "DarkTheme")
 
 local MainTab = Window:NewTab("Main")
-local MainSection = MainTab:NewSection("Chạy Script Mới")
+local MainSection = MainTab:NewSection("Execute New Script")
 
-MainSection:NewButton("Execute Script", "Chạy script mới từ Kenon Hub", function()
+MainSection:NewButton("Execute Script", "Run the new script from Kenon Hub", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/khoitongdz/Kenon-/refs/heads/main/KenonHubNewUpdated.lua"))()
 end)
 
-Window:NewTab("Menu"):NewSection("Thoát Script"):NewButton("Thoát", "Tắt Kenon Hub", function()
+Window:NewTab("Menu"):NewSection("Exit Script"):NewButton("Exit", "Close Kenon Hub", function()
     Library:ToggleUI()
 end)
